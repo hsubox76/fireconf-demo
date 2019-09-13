7bc9bf147f70b38927e40c335e22aa7dd5c6e3d7
Christina Holland <hsubox@gmail.com>
2019-09-13 16:05:19 -0700

    Fix update function

--- functions/index.js
+++ functions/index.js
@@ -29,19 +29,28 @@ exports.updateStocks = functions.pubsub.schedule('every 5 minutes').onRun(async 
   const stockMap = {};
   const currentDataRef = admin.firestore().doc('metadata/currentData');
   const currentDataSnap = await currentDataRef.get();
-  const { index, maxIndex } = currentDataSnap.data();
+  let { index, maxIndex } = currentDataSnap.data();
+  if (!index) {
+    index = 1;
+  }
   const cSnap = await admin.firestore().collection('historical').get();
   const batch = admin.firestore().batch();
+  let numSymbolsToUpdate = cSnap.size;
   cSnap.forEach(docSnap => {
     const symbol = docSnap.id;
     if (!docSnap.data()) { return; }
-    const dataPoint = docSnap.data().timeSeries[index || 1];
+    const dataPoint = docSnap.data().timeSeries[index];
     const previousDataPoint = docSnap.data().timeSeries[index - 1];
+    if (!dataPoint || !previousDataPoint) {
+      console.log(`Couldn't get data for ${symbol} at index ${index}`);
+      numSymbolsToUpdate--;
+      return;
+    }
     const delta = dataPoint.closeValue - previousDataPoint.closeValue;
     stockMap[symbol] = Object.assign({ delta }, dataPoint);
     const ref = admin.firestore().doc(`current/${symbol}`);
     batch.set(ref, stockMap[symbol]);
-    if (Object.keys(stockMap).length === cSnap.size) {
+    if (Object.keys(stockMap).length >= numSymbolsToUpdate) {
       let newIndex = index + 1;
       if (newIndex > maxIndex) {
         newIndex = 1;
